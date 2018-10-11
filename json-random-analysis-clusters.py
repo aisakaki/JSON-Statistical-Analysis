331a6c2886031e8b16f10cbd1c1416c2085dba7a
aisakaki <htt20081@126.com>
2018-10-11 11:11:08 +0800

    jsa_v2.0

--- json-random-analysis-clusters.py
+++ json-random-analysis-clusters.py
@@ -9,7 +9,8 @@ total_dict = {}
 dict_node_list = []
 list_node_list = []
 connection_dict = {}
-
+both_not_exist = {}
+ES_threshold = 0.5
 
 def generate_path_list(clusters_file, class_no, cluster_no, class_path):
     path_list = []
@@ -41,11 +42,13 @@ def generate_node_list(dic):
 
 
 def add_list_to_num_dict(a, lis, dic, allFeatureList):
+    global both_not_exist
     if lis in a.keys():
         for item in a[lis]:
             if item not in dic and item in allFeatureList:
                 if item not in total_dict:
                     total_dict[item] = 0   #错误点
+                    both_not_exist[item] = 0
                 a[item] = 1
                 dic.append(item)
             if item in dic and item in allFeatureList:
@@ -60,8 +63,8 @@ def add_list_to_connection_map(lis, key):
 
 
 def compare(a, b, allFeatureList):
+    global both_not_exist
     keys = list(set(a.keys()).union(set(b.keys())))
-
     for key in keys:
         # 判断是否在pickle中
         if key not in allFeatureList and key not in dict_node_list and key not in list_node_list:
@@ -69,6 +72,7 @@ def compare(a, b, allFeatureList):
         # 判断该键是否在总记录字典里，如果没有则新建，值为0
         if key not in total_dict:
             total_dict[key] = 0
+            both_not_exist[key] = 0
         # 如果key是list，则将a b中的list中的元素全部加入字典和keys里，值为0。这里在遍历中动态改变list
         if key in list_node_list:
             add_list_to_num_dict(a, key, keys, allFeatureList)
@@ -78,6 +82,7 @@ def compare(a, b, allFeatureList):
         if key in a and key in b:
             if a[key] == b[key]:
                 print("%-40s | SAME" % key)
+                both_not_exist[key] += 1
             else:
                 if key in dict_node_list:
                     compare(a[key], b[key], allFeatureList)
@@ -110,28 +115,51 @@ def random_choose_cluster(path_list):
             return d
 
 
-def integration(n):
-    global total_dict
+def integration(n, AD_threshold):
+    print("BNE is Both not exist")
+    global total_dict, both_not_exist
     for key in total_dict:
+        both_not_exist[key] = int(n - total_dict[key] - both_not_exist[key]) / n
         total_dict[key] = n - total_dict[key]
+
     total_list = sorted(total_dict.items(), key=lambda x: x[1])
     for (key, value) in total_list:
         same_rate = int(value) / n
+        if effective_same_judgement(same_rate, both_not_exist[key], AD_threshold , ES_threshold) is False:
+            continue
         if key not in dict_node_list and key not in list_node_list:
             if key not in connection_dict:
-                print('【'+key + "】 : %.2f" % same_rate)
+                if both_not_exist[key] > 0 and same_rate > AD_threshold:
+                    print('【'+key + "】 : %.2f  (BNE: %.2f)" % (same_rate, both_not_exist[key]))
+                else:
+                    print('【' + key + "】 : %.2f" % same_rate)
             else:
-                print(connection_dict[key] + '【' + key + "】 : %.2f" % same_rate)
+                if both_not_exist[key] > 0 and same_rate > AD_threshold:
+                    print(connection_dict[key] + '【' + key + "】 : %.2f  (BNE: %.2f)" % (same_rate, both_not_exist[key]))
+                else:
+                    print(connection_dict[key] + '【' + key + "】 : %.2f" % same_rate)
 
 
-# APah 和 BPath为要抽样比较的两个文件夹的路径，N为抽样样本个数，allFeaturePath为过滤特征文件
+# AD threshold:绝对不同门限；ES threshold:有效相似门限
+def effective_same_judgement(S, BNE, AD_threshold, ES_threshold):
+    if S < AD_threshold:
+        return True
+    ES = BNE / S
+    if ES > ES_threshold:
+        return False
+    else:
+        return True
+
+
+# APah 和 BPath为要抽样比较的两个文件夹的路径，N为抽样样本个数，allFeaturePath为过滤特征文件，AD threshold为绝对不同门限
 # 用于按照家族分析特征
-def run_path(APath, BPath, N, allFeaturePath):
-    global total_dict, dict_node_list, list_node_list, connection_dict
+def run_path(APath, BPath, N, allFeaturePath, AD_threshold):
+    global total_dict, dict_node_list, list_node_list, connection_dict, both_not_exist
     total_dict = {}
     dict_node_list = []
     list_node_list = []
     connection_dict = {}
+    both_not_exist = {}
     # pickle处理
     with open(allFeaturePath, 'rb') as f:
         allFeatureList = list(pickle.load(f))
@@ -155,23 +183,24 @@ def run_path(APath, BPath, N, allFeaturePath):
         compare(dict1, dict2, allFeatureList)
 
     print('\n=============SIMILARITY=============')
-    integration(N)
+    integration(N, AD_threshold)
 
 
-# clusters_file:聚类结果文件路径, class_no1：A的家族序号, cluster_no1：A的簇类序号, class_path：A特征家族的路径, class_no2：B的家族序号, cluster_no2：B的簇类序号, class_path：B特征家族的路径,N：抽样样本个数,allFeaturePath为过滤特征文件
+# clusters_file:聚类结果文件路径, class_no1：A的家族序号, cluster_no1：A的簇类序号, class_path：A特征家族的路径, class_no2：B的家族序号, cluster_no2：B的簇类序号, class_path：B特征家族的路径,N：抽样样本个数,allFeaturePath为过滤特征文件, AD threshold为绝对不同门限
 # 用于按照聚类之后簇分析特征
-def run_clusters(clusters_file, class_no1, cluster_no1, class_path1, class_no2, cluster_no2, class_path2, N, allFeaturePath):
-    global total_dict, dict_node_list, list_node_list, connection_dict
+def run_clusters(clusters_file, class_no1, cluster_no1, class_path1, class_no2, cluster_no2, class_path2, N, allFeaturePath, AD_threshold):
+    global total_dict, dict_node_list, list_node_list, connection_dict,both_not_exist
     total_dict = {}
     dict_node_list = []
     list_node_list = []
     connection_dict = {}
+    both_not_exist = {}
     # pickle处理
     with open(allFeaturePath, 'rb') as f:
         allFeatureList = list(pickle.load(f))
     l1 = generate_path_list(clusters_file, class_no1, cluster_no1, class_path1)
     l2 = generate_path_list(clusters_file, class_no2, cluster_no2, class_path2)
-    for i in range(1, N+1):     #闭合？
+    for i in range(1, N+1):
         #去重
         while True:
             APath_T = random_choose_cluster(l1)
@@ -191,7 +220,7 @@ def run_clusters(clusters_file, class_no1, cluster_no1, class_path1, class_no2, 
         compare(dict1, dict2, allFeatureList)
 
     print('\n=============SIMILARITY=============')
-    integration(N)
+    integration(N, AD_threshold)
 
 
-run_clusters('cl.txt', 0, 1, 'EXAMPLE', 1, 2, 'EXAMPLE', 10, r'S:\资料库\Desktop\聚类exp\allFeature1479.pickle')
+run_clusters('cl.txt', 0, 1, 'EXAMPLE', 1, 2, 'EXAMPLE', 10, r'S:\资料库\Desktop\聚类exp\allFeature1479.pickle', 0.5)
